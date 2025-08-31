ea9a3e0c6282a626e4e74028b85b6f5f7b42184d
popara03 <poparas03@gmail.com>
2025-08-31 16:44:08 +0200

    notifications completed (no api yet)

--- resources/js/components/custom/nav.tsx
+++ resources/js/components/custom/nav.tsx
@@ -3,15 +3,8 @@ import { Link, usePage } from "@inertiajs/react";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "../ui/button";
@@ -28,10 +21,10 @@ const Nav = () => {
     };
 
     // fetch last 10 notifications from backend
-    const notifications = [
-    { id: 1, post_id: 1, text: "Novi komentar od Marko Jovanovic na vašem postu Markaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", time: "5 min ago", is_read: true },
-    { id: 2, post_id: 2, text: "Diskusija \"Da li je velicina bitna?\" koju ste pratili je zatvorena.", time: "10 min ago", is_read: true },
-    { id: 3, post_id: 3, text: "Novi odgovor od Ana Nikolic na vašem postu Post broj tri", time: "30 min ago", is_read: true },
+    const [notifications, setNotifications] = useState([
+    { id: 1, post_id: 1, text: "Novi komentar od Marko Jovanovic na vašem postu Markaaaaaaaaaaaaa", time: "5 min ago", is_read: false },
+    { id: 2, post_id: 2, text: "Diskusija \"Da li je velicina bitna?\" koju ste pratili je zatvorena.", time: "10 min ago", is_read: false },
+    { id: 3, post_id: 3, text: "Novi odgovor od Ana Nikolic na vašem postu Post broj tri", time: "30 min ago", is_read: false },
     { id: 4, post_id: 4, text: "Novi komentar od Petar Petrovic na vašem postu Cetvrti post", time: "1 hour ago", is_read: true },
     { id: 5, post_id: 5, text: "Diskusija \"Kako nauciti React?\" koju ste pratili je zatvorena.", time: "2 hours ago", is_read: true },
     { id: 6, post_id: 6, text: "Novi odgovor od Jovana Jovanovic na vašem postu Post broj sest", time: "3 hours ago", is_read: true },
@@ -39,13 +32,48 @@ const Nav = () => {
     { id: 8, post_id: 8, text: "Novi odgovor od Jovana Jovanovic na vašem postu Post broj sest", time: "3 hours ago", is_read: true },
     { id: 9, post_id: 9, text: "Novi odgovor od Jovana Jovanovic na vašem postu Post broj sest", time: "3 hours ago", is_read: true },
     { id: 10, post_id: 10, text: "Novi odgovor od Jovana Jovanovic na vašem postu Post broj sest", time: "3 hours ago", is_read: true },
-    ];
+    ]);
 
-    // check if all notifications are read
+    // check if all notifications are read on load
     const [isAllRead, setIsAllRead] = useState(false);
+
+    const checkIsAllRead = () => {
+        setIsAllRead(notifications.every((n) => n.is_read));
+    };
+
     useEffect(() => {
-        setIsAllRead(notifications.every((n) => n.is_read))
+        checkIsAllRead();
     }, [notifications])
+
+    //child component handler for each notification status change
+    const handleCheckIsAllRead = (id:number, value:boolean) => {
+        let n  = notifications.find((n) => n.id === id);
+        
+        if(n){
+            n.is_read = value;
+            setNotifications([...notifications]);
+        }
+        else{
+            console.error("Notification with passed ID not found", id);
+        }
+    };
+
+    // child component handler for each notification removal
+    const handleRemoveDeletedNotification = (id:number) => {
+        setNotifications(notifications.filter((n) => n.id !== id));
+    };
+
+    // mark all notifications as read
+    const markAllAsRead = () => {
+        if(isAllRead) return;
+
+        // TODO: Make API call to mark all notifications as read, special endpoint /notifications/mark-all-as-read
+        
+        notifications.forEach((n) => n.is_read = true);
+        setNotifications([...notifications]);
+        
+        console.log("All notifications marked as read");
+    }
 
     return (
     <nav className="z-50 sticky top-0 left-0 flex justify-between items-center w-full h-[60px] px-4 font-jersey bg-primary *:text-secondary a:font-jersey border-b border-secondary/10 ease-in-out transition-all duration-300">
@@ -53,75 +81,112 @@ const Nav = () => {
             Organizimo
         </Link>
 
+        {/* notifications (mobile), burger icon */}
+        <div className="w-fit flex items-center gap-8">
+            {user && (
+                /* notifications (mobile) */
+                <Link href="/notifications" className="block md:hidden font-jersey w-full h-fit ">
+                    {isAllRead ? (
+                    <svg className="w-8 h-8 cursor-pointer hover:opacity-50 *:fill-secondary" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 26.6667V29.3333H18.6667V30.6667H13.3333V29.3333H12V26.6667H20Z" fill="#EEEEEE"/>
+                    <path d="M29.3333 22.6667V24H28V25.3333H3.99996V24H2.66663V22.6667H3.99996V21.3333H5.33329V18.6667H6.66663V10.6667H7.99996V8H9.33329V6.66667H10.6666V5.33333H13.3333V4H14.6666V1.33333H17.3333V4H18.6666V5.33333H21.3333V6.66667H22.6666V8H24V10.6667H25.3333V18.6667H26.6666V21.3333H28V22.6667H29.3333Z" fill="#EEEEEE"/>
+                    </svg>
+                    ) : (
+                    <svg className="w-8 h-8 cursor-pointer hover:opacity-50 *:fill-accent-purple" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 26.6667V29.3333H18.6667V30.6667H13.3333V29.3333H12V26.6667H20Z"/>
+                    <path d="M29.3333 22.6667V24H28V25.3333H3.99996V24H2.66663V22.6667H3.99996V21.3333H5.33329V18.6667H6.66663V10.6667H7.99996V8H9.33329V6.66667H10.6666V5.33333H13.3333V4H14.6666V1.33333H17.3333V4H18.6666V5.33333H21.3333V6.66667H22.6666V8H24V10.6667H25.3333V18.6667H26.6666V21.3333H28V22.6667H29.3333Z" fill="#EEEEEE"/>
+                    </svg>
+                    )}
+                </Link>
+            )}
+
+            {/* burger */}
+            <button onClick={handleBurgerClick} className={`md:hidden flex flex-col items-end cursor-pointer hover:opacity-50 ${isMenuOpen ? 'activeBurger' : ''}`}>
+                <span className="block w-8 h-1 bg-secondary mb-1 transition-all ease-in-out duration-300"></span>
+                <span className="block w-8 h-1 bg-secondary mb-1 transition-all ease-in-out duration-300"></span>
+                <span className="block w-8 h-1 bg-secondary transition-all ease-in-out duration-300"></span>
+            </button>
+        </div>
+
         <ul className={`w-10/12 min-h-[calc(100vh-60px)] md:min-h-auto bg-primary md:bg-transparent md:w-auto md:h-auto fixed md:static top-[60px] md:top-0 left-full md:left-0 flex flex-col md:flex-row items-center md:gap-8 *:text-secondary transition-all duration-300 shadow-2xl ${isMenuOpen ? "openMenu" : ""}`}>
             {!user ? (
             <>
-                <li><Link href="/login" className="font-jersey block w-full h-fit md:w-auto p-4 md:p-0 bg-primary md:bg-transparent border-b md:border-0 border-secondary/10 hover:opacity-50">Login</Link></li>
-                <li><Link href="/register" className="font-jersey block w-full h-fit md:w-auto p-4 md:p-0 bg-primary md:bg-transparent border-b md:border-0 border-secondary/10 hover:opacity-50">Register</Link></li>
+                <li className="w-full"><Link href="/login" className="font-jersey block w-full h-fit md:w-auto p-4 md:p-0 bg-primary md:bg-transparent border-b md:border-0 border-secondary/10 hover:opacity-50">Login</Link></li>
+                <li className="w-full"><Link href="/register" className="font-jersey block w-full h-fit md:w-auto p-4 md:p-0 bg-primary md:bg-transparent border-b md:border-0 border-secondary/10 hover:opacity-50">Register</Link></li>
             </>
             ) : (
             <>
                 {/* dashboard */}
                 <li className="w-full">
+                    {/* desktop */}
                     <Link href="/dashboard" className="hidden md:block">
                         <img src="/icons/dashboard.svg" alt="Dashboard" className="w-8 h-8"/>
                     </Link>
 
+                    {/* mobile */}
                     <Link href="/dashboard" className="block md:hidden font-jersey w-full h-fit md:w-auto p-4 md:p-0 bg-primary md:bg-transparent border-b md:border-0 border-secondary/10">
                         Dashboard
                     </Link>
                 </li>
 
                 {/* notifications */}
-                <li className="w-full">
-                    <div className="hidden md:block">
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                                {/* if has notifications, add class for fill change - notify */}
-                                { isAllRead ? (
-                                    <svg className="w-8 h-8 cursor-pointer hover:opacity-50 *:fill-secondary" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M20 26.6667V29.3333H18.6667V30.6667H13.3333V29.3333H12V26.6667H20Z" fill="#EEEEEE"/>
-                                    <path d="M29.3333 22.6667V24H28V25.3333H3.99996V24H2.66663V22.6667H3.99996V21.3333H5.33329V18.6667H6.66663V10.6667H7.99996V8H9.33329V6.66667H10.6666V5.33333H13.3333V4H14.6666V1.33333H17.3333V4H18.6666V5.33333H21.3333V6.66667H22.6666V8H24V10.6667H25.3333V18.6667H26.6666V21.3333H28V22.6667H29.3333Z" fill="#EEEEEE"/>
-                                    </svg>
-                                ) : (
-                                    <svg className="w-8 h-8 cursor-pointer hover:opacity-50 *:fill-accent-purple" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M20 26.6667V29.3333H18.6667V30.6667H13.3333V29.3333H12V26.6667H20Z"/>
-                                    <path d="M29.3333 22.6667V24H28V25.3333H3.99996V24H2.66663V22.6667H3.99996V21.3333H5.33329V18.6667H6.66663V10.6667H7.99996V8H9.33329V6.66667H10.6666V5.33333H13.3333V4H14.6666V1.33333H17.3333V4H18.6666V5.33333H21.3333V6.66667H22.6666V8H24V10.6667H25.3333V18.6667H26.6666V21.3333H28V22.6667H29.3333Z" fill="#EEEEEE"/>
-                                    </svg>
-                                )}
-                            </DropdownMenuTrigger>
+                <li className="hidden md:block w-full">
+                    {/* desktop */}
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            {/* if has notifications, add class for fill change - notify */}
+                            { isAllRead ? (
+                                <svg className="w-8 h-8 cursor-pointer hover:opacity-50 *:fill-secondary" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M20 26.6667V29.3333H18.6667V30.6667H13.3333V29.3333H12V26.6667H20Z" fill="#EEEEEE"/>
+                                <path d="M29.3333 22.6667V24H28V25.3333H3.99996V24H2.66663V22.6667H3.99996V21.3333H5.33329V18.6667H6.66663V10.6667H7.99996V8H9.33329V6.66667H10.6666V5.33333H13.3333V4H14.6666V1.33333H17.3333V4H18.6666V5.33333H21.3333V6.66667H22.6666V8H24V10.6667H25.3333V18.6667H26.6666V21.3333H28V22.6667H29.3333Z" fill="#EEEEEE"/>
+                                </svg>
+                            ) : (
+                                <svg className="w-8 h-8 cursor-pointer hover:opacity-50 *:fill-accent-purple" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M20 26.6667V29.3333H18.6667V30.6667H13.3333V29.3333H12V26.6667H20Z"/>
+                                <path d="M29.3333 22.6667V24H28V25.3333H3.99996V24H2.66663V22.6667H3.99996V21.3333H5.33329V18.6667H6.66663V10.6667H7.99996V8H9.33329V6.66667H10.6666V5.33333H13.3333V4H14.6666V1.33333H17.3333V4H18.6666V5.33333H21.3333V6.66667H22.6666V8H24V10.6667H25.3333V18.6667H26.6666V21.3333H28V22.6667H29.3333Z" fill="#EEEEEE"/>
+                                </svg>
+                            )}
+                        </DropdownMenuTrigger>
 
-                            <DropdownMenuContent
-                            align="end"
-                            className="w-96 max-h-96 h-96 p-0 pb-10 flex flex-col bg-primary rounded-none rounded-b-2xl overflow-y-auto scrollbar"
-                            sideOffset={14}
-                            >
-                                {notifications.map((notification: NotificationProps) => (
-                                    <DropdownMenuItem key={notification.id} className="p-0 flex flex-col gap-2 rounded-none border-b border-secondary/10 hover:bg-secondary/5">
-                                        <Notification {...notification} />
-                                    </DropdownMenuItem>
-                                    ))}
+                        <DropdownMenuContent
+                        align="end"
+                        className="w-96 max-h-96 h-96 p-0 pb-10 flex flex-col bg-primary rounded-none rounded-b-2xl overflow-y-auto scrollbar"
+                        sideOffset={14}
+                        >
+                            {notifications.length === 0 && (
+                                <p className="w-full h-full flex justify-center items-center px-4 py-2 text-secondary text-center">Crickets… no notifications here 🦗</p>
+                            )}
 
-                                    <DropdownMenuLabel className="mt-auto absolute bottom-0 left-0 z-10 w-full h-10 p-0 flex justify-between items-center rounded-b-2xl overflow-hidden backdrop-blur-3xl">
-                                        <Link href="/notifications" className="bg-accent-blue w-full h-full px-4 py-2 flex justify-center items-center text-center text-secondary rounded-none cursor-pointer hover:opacity-100 hover:bg-accent-blue/75">
-                                            See all notifications
-                                        </Link>
+                            {notifications.map((notification) => (
+                                <DropdownMenuItem key={notification.id} className="p-0 flex flex-col gap-2 rounded-none border-b border-secondary/10 hover:bg-secondary/5">
+                                    <Notification
+                                    {...notification}
+                                    handleCheckIsAllRead={handleCheckIsAllRead}
+                                    handleRemoveDeletedNotification={handleRemoveDeletedNotification}
+                                    />
+                                </DropdownMenuItem>
+                                ))}
 
-                                        <Button className="bg-accent-lime w-full h-full px-4 py-2 flex justify-center items-center text-center text-secondary rounded-none cursor-pointer hover:bg-accent-lime/75" onClick={() => console.log("Mark all as read")}>
-                                            Mark all as read
-                                        </Button>
-                                    </DropdownMenuLabel>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </div>
+                                <DropdownMenuLabel className="mt-auto absolute bottom-0 left-0 z-10 w-full h-10 p-0 flex justify-between items-center rounded-b-2xl overflow-hidden backdrop-blur-3xl">
+                                    <Link href="/notifications" className="bg-accent-blue w-full h-full px-4 py-2 flex justify-center items-center text-center text-secondary rounded-none cursor-pointer hover:opacity-100 hover:bg-accent-blue/75 border-r border-secondary/10">
+                                        See all
+                                    </Link>
 
-                    <Link href="/notifications" className="block md:hidden font-jersey w-full h-fit md:w-auto p-4 md:p-0 bg-primary md:bg-transparent border-b md:border-0 border-secondary/10">
-                        Notifications
-                    </Link>
+                                    <Button
+                                    className="bg-accent-lime w-full h-full px-4 py-2 flex justify-center items-center text-center text-secondary rounded-none cursor-pointer hover:bg-accent-lime/75"
+                                    onClick={markAllAsRead}>
+                                        Mark all as read
+                                    </Button>
+                                </DropdownMenuLabel>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+
+                    {/* on mobile it's outside of dropdown */}
                 </li>
-                
-                {/* Avatar menu - for desktop */}
+
+                {/* Avatar menu*/}
                 <li className="w-full">
+                    {/* desktop */}
                     <div className="hidden md:block">
                         <DropdownMenu>
                             <DropdownMenuTrigger asChild>
@@ -154,25 +219,21 @@ const Nav = () => {
                             </DropdownMenuContent>
                         </DropdownMenu>
                     </div>
-
+                    
+                    {/* mobile */}
                     <div className="block md:hidden text-secondary">
                         <Link href="/profile" className="font-jersey block w-full h-fit md:w-auto p-4 md:p-0 bg-primary md:bg-transparent border-b md:border-0 border-secondary/10">Profile</Link>
+                        
                         {user.isAdmin && (
                             <Link href="/admin" className="font-jersey block w-full h-fit md:w-auto p-4 md:p-0 bg-primary md:bg-transparent border-b md:border-0 border-secondary/10">Admin panel</Link>
                         )}
+
                         <Link href="/logout" className="font-jersey block w-full h-fit md:w-auto p-4 md:p-0 bg-primary md:bg-transparent border-b md:border-0 border-secondary/10">Logout</Link>
                     </div>
                 </li>
             </>
             )}
         </ul>
-
-        {/* burger */}
-        <button onClick={handleBurgerClick} className={`md:hidden flex flex-col items-end cursor-pointer hover:opacity-50 ${isMenuOpen ? 'activeBurger' : ''}`}>
-            <span className="block w-8 h-1 bg-secondary mb-1 transition-all ease-in-out duration-300"></span>
-            <span className="block w-8 h-1 bg-secondary mb-1 transition-all ease-in-out duration-300"></span>
-            <span className="block w-8 h-1 bg-secondary transition-all ease-in-out duration-300"></span>
-        </button>
     </nav>
   )
 }
--- resources/js/components/custom/notification.tsx
+++ resources/js/components/custom/notification.tsx
@@ -1,5 +1,6 @@
 import {Link} from '@inertiajs/react'
 import {Button} from '@/components/ui/button'
+import { useState } from 'react'
 
 export type NotificationProps = {
     id: number
@@ -7,31 +8,75 @@ export type NotificationProps = {
     text: string
     time: string
     is_read: boolean
+    handleCheckIsAllRead: (id:number, value:boolean) => void
+    handleRemoveDeletedNotification: (id:number) => void
 }
 
-const Notification = ( props: NotificationProps ) => {
-  return (
-    <Link href={`/posts/${props.post_id}`} className="w-full px-4 py-2 flex justify-between items-center gap-2 hover:opacity-100">
+const Notification = ( props: NotificationProps) => {
+    const markNotificationAsRead = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+
+        // TODO: Make API call to mark notification as read
+
+            props.handleCheckIsAllRead(props.id, !props.is_read);
+
+        console.log("Mark notification as read", props.id)
+    }
+
+    const deleteNotification = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+
+        // TODO: Make API call to delete notification
+
+            props.handleRemoveDeletedNotification(props.id);
+
+        console.log("Delete notification", props.id)
+    }
+
+    return (
+    <Link
+    href={`/posts/${props.post_id}`}
+    className={`w-full px-4 py-2 flex justify-between items-center gap-2 hover:opacity-100 ${props.is_read ? "" : "bg-accent-purple/50"}`}
+    >
         <div className="min-w-0 w-full flex flex-col gap-2">
             <span className="text-sm text-secondary line-clamp-2">{props.text}</span>
             <span className="text-xs text-secondary/50">{props.time}</span>
         </div>
 
         <div className="flex gap-2">
-            <Button size='icon' className="bg-secondary/5 cursor-pointer" onClick={() => console.log("Mark as read", props.id)}>
-                <svg className="!w-6 !h-6 *:fill-accent-lime" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M17 11H16V13H17V11Z" fill="black"/>
-                <path d="M16 13V15H15V16H13V15H14V14H15V13H16Z" fill="black"/>
-                <path d="M16 9V11H15V10H14V9H13V8H15V9H16Z" fill="black"/>
-                <path d="M13 16H11V17H13V16Z" fill="black"/>
-                <path d="M11 15V16H9V15H8V13H9V14H10V15H11Z" fill="black"/>
-                <path d="M13 7V8H12V11H11V12H8V13H7V11H8V9H9V8H11V7H13Z" fill="black"/>
-                <path d="M22 11V9H21V8H20V7H19V6H17V5H7V6H5V7H4V8H3V9H2V11H1V13H2V15H3V16H4V17H5V18H7V19H17V18H19V17H20V16H21V15H22V13H23V11H22ZM21 14H20V15H19V16H18V17H16V18H8V17H7V16H5V15H4V14H3V10H4V9H5V8H6V7H8V6H16V7H18V8H19V9H20V10H21V14Z" fill="black"/>
-                </svg>
+            <Button size='icon'
+            className="bg-secondary/5 cursor-pointer"
+            onClick={markNotificationAsRead}
+            >
+                {!props.is_read ? (
+                    <svg className="!w-6 !h-6 *:fill-secondary" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17 11H16V13H17V11Z" fill="black"/>
+                    <path d="M16 13V15H15V16H13V15H14V14H15V13H16Z" fill="black"/>
+                    <path d="M16 9V11H15V10H14V9H13V8H15V9H16Z" fill="black"/>
+                    <path d="M13 16H11V17H13V16Z" fill="black"/>
+                    <path d="M11 15V16H9V15H8V13H9V14H10V15H11Z" fill="black"/>
+                    <path d="M13 7V8H12V11H11V12H8V13H7V11H8V9H9V8H11V7H13Z" fill="black"/>
+                    <path d="M22 11V9H21V8H20V7H19V6H17V5H7V6H5V7H4V8H3V9H2V11H1V13H2V15H3V16H4V17H5V18H7V19H17V18H19V17H20V16H21V15H22V13H23V11H22ZM21 14H20V15H19V16H18V17H16V18H8V17H7V16H5V15H4V14H3V10H4V9H5V8H6V7H8V6H16V7H18V8H19V9H20V10H21V14Z" fill="black"/>
+                    </svg>
+                ) : (
+                    <svg className="!w-6 !h-6 *:fill-secondary" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15 13H16V15H15V16H13V15H14V14H15V13Z" fill="black"/>
+                    <path d="M17 11H16V13H17V11Z" fill="black"/>
+                    <path d="M23 11V13H22V15H21V16H20V17H19V18H17V19H9V18H16V17H18V16H19V15H20V14H21V10H20V9H19V8H21V9H22V11H23Z" fill="black"/>
+                    <path d="M2 13H1V11H2V9H3V8H4V7H5V6H7V5H15V6H8V7H6V8H5V9H4V10H3V14H4V15H5V16H3V15H2V13Z" fill="black"/>
+                    <path d="M13 7V8H12V9H11V10H10V11H9V12H8V13H7V11H8V9H9V8H11V7H13Z" fill="black"/>
+                    <path d="M9 17H8V18H7V19H6V20H5V21H4V22H3V21H2V20H3V19H4V18H5V17H6V16H7V15H8V14H9V13H10V12H11V11H12V10H13V9H14V8H15V7H16V6H17V5H18V4H19V3H20V2H21V3H22V4H21V5H20V6H19V7H18V8H17V9H16V10H15V11H14V12H13V13H12V14H11V15H10V16H9V17Z" fill="black"/>
+                    <path d="M13 16H11V17H13V16Z" fill="black"/>
+                    </svg>
+                )}
             </Button>
 
-            <Button size='icon' className="bg-secondary/5 cursor-pointer" onClick={() => console.log("Delete", props.id)}>
-                <svg className="!w-6 !h-6 *:fill-accent-red" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <Button
+            size='icon'
+            className="bg-secondary/5 cursor-pointer"
+            onClick={deleteNotification}
+            >
+                <svg className="!w-6 !h-6 *:fill-secondary" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path  d="M20 6V14H19V22H18V23H6V22H5V14H4V6H20Z" fill="black"/>
                 <path  d="M21 3V5H3V3H4V2H9V1H15V2H20V3H21Z" fill="black"/>
                 </svg>
