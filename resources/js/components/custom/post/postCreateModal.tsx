c9504381a320c5b7c38f723be72ad4870478f3c2
popara03 <poparas03@gmail.com>
2025-09-16 21:29:53 +0200

    save

--- resources/js/components/custom/post/postCreateModal.tsx
+++ resources/js/components/custom/post/postCreateModal.tsx
@@ -23,16 +23,11 @@ import FileUploader from "@/components/custom/fileUploader";
 import { Form } from '@inertiajs/react'
 import {Label} from '@/components/ui/label'
 import {Input} from '@/components/ui/input'
+import {Textarea} from '@/components/ui/textarea'
 import InputError from '@/components/input-error'
 import { Button } from '@/components/ui/button'
-// mergde these imports
-import { Heading1, Heading2, Heading3, LoaderCircle, Underline, Bold, Italic, Strikethrough, AlignLeft, AlignCenter, AlignRight, List, ListOrdered, Link as LinkIcon, Unlink, Undo, Redo } from "lucide-react";
+import { LoaderCircle } from "lucide-react";
 import { toast } from 'sonner';
-
-import { Editor, useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import TextAlign from '@tiptap/extension-text-align'
-import { Toggle } from '@radix-ui/react-toggle';
 
 type PostModalProps = {
     isOpen: boolean,
@@ -44,219 +39,9 @@ type PostModalProps = {
     onUpdate: (group: any) => void
 }
 
-function MenuBar({ editor }: { editor: Editor }) {
-    if (!editor) {
-        return null
-    }
-
-    const Options = [
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <Heading1 className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
-      pressed: editor.isActive("heading", { level: 1 }),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <Heading2 className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
-      pressed: editor.isActive("heading", { level: 2 }),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <Heading3 className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
-      pressed: editor.isActive("heading", { level: 3 }),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <Bold className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().toggleBold().run(),
-      pressed: editor.isActive("bold"),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <Italic className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().toggleItalic().run(),
-      pressed: editor.isActive("italic"),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <Strikethrough className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().toggleStrike().run(),
-      pressed: editor.isActive("strike"),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <Underline className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().toggleUnderline().run(),
-      pressed: editor.isActive("underline"),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <AlignLeft className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().setTextAlign("left").run(),
-      pressed: editor.isActive({ textAlign: "left" }),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <AlignCenter className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().setTextAlign("center").run(),
-      pressed: editor.isActive({ textAlign: "center" }),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <AlignRight className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().setTextAlign("right").run(),
-      pressed: editor.isActive({ textAlign: "right" }),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <List className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().toggleBulletList().run(),
-      pressed: editor.isActive("bulletList"),
-    },
-    {
-      icon: (
-        <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-          <ListOrdered className="size-6" />
-        </div>
-      ),
-      onClick: () => editor.chain().focus().toggleOrderedList().run(),
-      pressed: editor.isActive("orderedList"),
-    },
-    {
-        icon: (
-          <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-            <LinkIcon className="size-6" />
-          </div>
-        ),
-        onClick: (e: any) => editor.chain().focus().setLink({ href: e.currentTarget.value, target: "_blank" }).run(),
-        pressed: editor.isActive("link"),
-    },
-    {
-        icon: (
-          <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-            <Unlink className="size-6" />
-            </div>
-        ),
-        onClick: () => editor.chain().focus().unsetLink().run(),
-        pressed: editor.isActive("link"),
-    },
-    {
-        icon: (
-            <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-                <Undo className="size-6" />
-            </div>
-        ),
-        onClick: () => editor.chain().focus().undo().run(),
-        pressed: editor.isActive("undo"),
-    },
-    {
-        icon: (
-            <div className="p-1 flex justify-center items-center rounded-md bg-tertiary">
-                <Redo className="size-6" />
-            </div>
-        ),
-        onClick: () => editor.chain().focus().redo().run(),
-        pressed: editor.isActive("redo"),
-    }
-  ];
-
-  return (
-    <div className="flex flex-wrap gap-2">
-        {Options.map((option, index) => (
-        <Toggle
-          key={index}
-          pressed={option.pressed}
-          onPressedChange={option.onClick}
-        >
-          {option.icon}
-        </Toggle>
-      ))}
-    </div>
-  )
-}
-
 const PostCreateModal = ({isOpen, onIsOpenChange, postForEdit, groups, activeGroupId, onCreate, onUpdate}: PostModalProps) => {
     // Content
     const [content, setContent] = useState<string>(postForEdit?.content || 'KURAC');
-
-    const editor = useEditor({
-        editorProps: {
-            attributes: {
-                class: 'bg-primary *:!text-secondary w-full h-56 p-4 rounded-md overflow-y-auto scrollbar focus:text-secondary',
-            },
-        },
-        extensions: [
-            StarterKit.configure({
-                link: {
-                    openOnClick: false,
-                    HTMLAttributes: {
-                        class: 'text-blue-500 underline',
-                    },
-                },
-                heading: {
-                    levels: [1, 2, 3],
-                    HTMLAttributes: {
-                        class: 'text-secondary font-bold mt-0.5 mb-2',
-                    },
-                },
-                bulletList: {
-                    HTMLAttributes: { class: 'text-secondary list-disc ml-6 mb-2' },
-                },
-                orderedList: {
-                    HTMLAttributes: { class: 'text-secondary list-decimal ml-6 mb-2' },
-                },
-                listItem: {
-                    HTMLAttributes: { class: 'mb-1' },
-                },
-                paragraph: {
-                    HTMLAttributes: { class: 'text-secondary mb-2' },
-                },
-            }),
-            TextAlign.configure({
-                types: ['heading', 'paragraph'],
-            }),
-        ],
-        content: content,
-        onUpdate: (content: any) => {
-            setContent(content);
-        },
-    });
 
     // group
     const [group, setGroup] = useState<any>(null);
@@ -327,7 +112,9 @@ const PostCreateModal = ({isOpen, onIsOpenChange, postForEdit, groups, activeGro
                                             name="title"
                                             placeholder='Post title'
                                             required
-                                            defaultValue={postForEdit?.title || ''} />
+                                            defaultValue={postForEdit?.title || ''}
+                                            className='px-4'
+                                        />
                                         <InputError message={errors.title} />
                                     </div>
 
@@ -337,9 +124,14 @@ const PostCreateModal = ({isOpen, onIsOpenChange, postForEdit, groups, activeGro
                                         <div className="flex flex-col gap-4">
                                             <div className="w-full flex flex-col gap-2">
                                                 {/* editor */}
-                                                <MenuBar editor={editor} />
-                                                <EditorContent 
-                                                editor={editor} 
+                                                <Textarea
+                                                    id="content"
+                                                    name="content"
+                                                    placeholder='Post content'
+                                                    required
+                                                    value={content}
+                                                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value)}
+                                                    className='w-full min-h-[200px] p-4 !bg-secondary text-primary border border-primary/10 focus:border-primary rounded-md overflow-y-auto scrollbar'
                                                 />
 
                                                 <InputError message={errors.content} />
