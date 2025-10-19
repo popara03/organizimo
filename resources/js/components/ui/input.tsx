debb3b8ad4e3590f3f0b0bd4b8678fd45903bc0d
popara03 <poparas03@gmail.com>
2025-10-20 01:43:36 +0200

    added show/hide lolic on pw inputs

--- resources/js/components/ui/input.tsx
+++ resources/js/components/ui/input.tsx
@@ -3,18 +3,44 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+  const [showPassword, setShowPassword] = React.useState(false)
+
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
+    type === "password" ? (
+      <div className="relative">
+        <input
+        type={type== 'password' ? (showPassword ? 'text' : "password") : type}
+        data-slot="input"
+        className={cn(
+          "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+          "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+          className
+        )}
+          {...props}
+        />
+
+        <button className="absolute inset-y-0 right-2 flex items-center cursor-pointer" type="button" onClick={() => setShowPassword(!showPassword)}>
+          {!showPassword ? (
+            <img src="/icons/eye.svg" alt="eye" className="size-6" />
+          ) : (
+            <img src="/icons/eye-cross.svg" alt="eye crossed" className="size-6" />
+          )}
+        </button>
+      </div>
+    ) : (
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(
+          "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+          "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+          className
+        )}
+        {...props}
+      />
+    )
   )
 }
 
